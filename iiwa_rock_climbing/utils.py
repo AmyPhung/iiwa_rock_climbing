34016ca8a2c98d7790b806f459ac0dd6a81887b1
Amy <amyngph@gmail.com>
2021-12-04 10:50:23 -0500

    Wrote servo controller, untested

--- iiwa_rock_climbing/utils.py
+++ iiwa_rock_climbing/utils.py
@@ -21,3 +21,30 @@ def CreatePoseFromVector(q_vec, p_vec):
     q_quat = Quaternion(q_vec)
     rot = RotationMatrix(q_quat)
     return RigidTransform(rot, p_vec)
+
+def ComputeEulerFromQuat(q_vec):
+    q_vec = np.array(q_vec)
+    q_quat = Quaternion(q_vec)
+    rpy = RollPitchYaw(q_quat)
+    return rpy.vector()
+
+def ComputeAngleComplement(r_vec):
+    """Ensures all angles are less than 180 degrees"""
+    r_vec = np.array(r_vec)
+    
+    while max(r_vec)>np.pi:
+        r_vec[r_vec>np.pi] -= 2*np.pi
+    while min(r_vec)<-np.pi:
+        r_vec[r_vec<-np.pi] += 2*np.pi
+    return r_vec
+    
+def ComputeAngleOffset(r_vec1, r_vec2):
+    """Computes delta between two euler angles, ensuring that the 
+    delta remains 'continuous' 
+    
+    Usage note: set r_vec1 to desired, r_vec2 to estimated"""
+    r_vec1 = ComputeAngleComplement(r_vec1)
+    r_vec2 = ComputeAngleComplement(r_vec2)
+    
+    delta = ComputeAngleComplement(r_vec1 - r_vec2)
+    return delta
